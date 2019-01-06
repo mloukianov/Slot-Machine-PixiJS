8a1adabe64c96486c9249b328f4a9dfb091c9b3d
Anastasia Seraciov <anastasia.seraciov@yahoo.com>
2019-01-06 01:39:05 +0000

    Add responsivness

--- js/app.js
+++ js/app.js
@@ -14,12 +14,138 @@
 
 const app = new PIXI.Application(640, 360, {
     transparent: true,
-    autoResize: true,
+    //autoResize: true,
     antialias: true,
-    resolution: 1,
+   // resolution: window.devicePixelRatio || 1,
 });
 
+let canvas = document.querySelector('canvas');
+
+/* function resizeWindow() {
+    // Our canvas must cover full height of screen
+    // regardless of the resolution
+    let height = window.innerHeight;
+
+    // So we need to calculate the proper scaled width
+    // that should work well with every resolution
+    
+    let ratio = canvas.width / canvas.height;
+    let width = height * ratio;
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+}
+
+window.addEventListener('load', resizeWindow, false);
+window.addEventListener('resize', resizeWindow, false); */
+function scaleToWindow(eleDict, getWindow, getDocument, backgroundColor) {
+    return (canvas) => {
+        const container = document.querySelector(eleDict.containerSel);
+        const _window = getWindow();
+        const _document = getDocument();
+        var scaleX, scaleY, scale, center;
+
+        const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !_window.MSStream;
+
+        const getWWidth = () => ((iOS) ? _window.screen.width : _window.innerWidth ||
+            _document._documentElement.clientWidth ||
+            _document.body.clientWidth);
+
+        const getWHeight = () => ((iOS) ? _window.screen.height : _window.innerHeight ||
+            _document._documentElement.clientHeight ||
+            _document.body.clientHeight);
+
+
+        //1. Scale the container to the correct size
+        //Figure out the scale amount on each axis
+        scaleX = getWWidth() / canvas.offsetWidth;
+        scaleY = getWHeight() / canvas.offsetHeight;
+
+        //Scale the container based on whichever value is less: `scaleX` or `scaleY`
+        scale = Math.min(scaleX, scaleY);
+        container.style.transformOrigin = "0 0";
+        container.style.transform = "scale(" + scale + ")";
+        if (eleDict.childSelArr) {
+            containerSelectors.childSelArr.map((sel) => {
+                document.querySelector(sel).style.height = canvas.offsetHeight;
+                document.querySelector(sel).style.width = canvas.offsetWidth;
+            })
+        }
+        container.style.height = canvas.offsetHeight;
+        container.style.width = canvas.offsetWidth;
+
+        //2. Center the container.
+        //Decide whether to center the container vertically or horizontally.
+        //Wide containeres should be centered vertically, and
+        //square or tall containeres should be centered horizontally
+        if (canvas.offsetWidth > canvas.offsetHeight) {
+            if (canvas.offsetWidth * scale < getWWidth()) {
+                center = "horizontally";
+            } else {
+                center = "vertically";
+            }
+        } else {
+            if (canvas.offsetHeight * scale < getWHeight()) {
+                center = "vertically";
+            } else {
+                center = "horizontally";
+            }
+        }
+
+        //Center horizontally (for square or tall containeres)
+        var margin;
+        if (center === "horizontally") {
+            margin = (getWWidth() - canvas.offsetWidth * scale) / 2;
+            container.style.marginTop = 0 + "px";
+            container.style.marginBottom = 0 + "px";
+            container.style.marginLeft = margin + "px";
+            container.style.marginRight = margin + "px";
+        }
+
+        //Center vertically (for wide containeres)
+        if (center === "vertically") {
+            margin = (getWHeight() - container.offsetHeight * scale) / 2;
+            container.style.marginTop = margin + "px";
+            container.style.marginBottom = margin + "px";
+            container.style.marginLeft = 0 + "px";
+            container.style.marginRight = 0 + "px";
+        }
+
+        //3. Remove any padding from the container  and body and set the canvas
+        //display style to "block"
+        container.style.paddingLeft = 0 + "px";
+        container.style.paddingRight = 0 + "px";
+        container.style.paddingTop = 0 + "px";
+        container.style.paddingBottom = 0 + "px";
+        container.style.display = "block";
+        container.style.width = canvas.offsetWidth + 'px';
+        container.style.height = canvas.offsetHeight + 'px';
+
+        //4. Set the color of the HTML body background
+        if (backgroundColor) {
+            _document.body.style.backgroundColor = backgroundColor;
+        }
+
+        //Fix some quirkiness in scaling for Safari
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.indexOf("safari") != -1) {
+            if (ua.indexOf("chrome") > -1) {
+                // Chrome
+            } else {
+                // Safari
+                //container.style.maxHeight = "100%";
+                //container.style.minHeight = "100%";
+            }
+        }
+
+        //5. Return the `scale` value. This is important, because you'll nee this value
+        //for correct hit testing between the pointer and sprites
+        return scale;
+    }
+}
+
 document.body.appendChild(app.view);
+
 
 class Resources {
     constructor(balance, stake, win) {
@@ -240,7 +366,8 @@ function onAssetsLoaded() {
     reelContainer.y = margin * 2.8;
     reelContainer.x = 200;
     const top = new PIXI.Graphics();
-    top.beginFill(0, 1);
+    top.beginFill(0xFF3300);
+    //.beginFill(0, 1);
     top.drawRect(0, 0, app.screen.width, margin);
     const bottom = new PIXI.Graphics();
     bottom.beginFill(0, 1);
